8ae0e60dfe1eadeb7b5653b20696710e860ee9bf
ivenbews <as_lod@mail.com>
2021-11-10 18:00:33 +0500

    db was changed

--- src/main/java/com/example/bookcatalog/service/BookService.java
+++ src/main/java/com/example/bookcatalog/service/BookService.java
@@ -3,7 +3,6 @@ package com.example.bookcatalog.service;
 import com.example.bookcatalog.model.Book;
 import com.example.bookcatalog.model.Genre;
 import com.example.bookcatalog.repository.BookRepository;
-import com.example.bookcatalog.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
